27db611ae6b9b33eba7922959d0ff14a85cf5486
HabibaYossre <queenrody24@gmail.com>
2024-08-26 09:22:32 +0300

    Practice of Session 6

---
+++ Session 6/Practice.sh
@@ -0,0 +1,63 @@
+
+# 1. Variables
+# Define a variable to store a greeting message
+
+
+# Define a variable for the user's name
+
+# Define a variable for the course name
+
+# Print a welcome message using the variables above
+
+#echo "________, ________! Welcome to your ________ practice session."
+
+# 2. Take User Input
+# Prompt the user to enter their favorite number
+
+#echo "Please enter your favorite number:"
+
+
+# 3. Arithmetic Expansion
+# Calculate the double of the user's favorite number in "double_number" Variable
+
+
+# Calculate the square of the favorite number in "square_number" Variable
+
+
+# Find the remainder when the favorite number is divided by 2 in "remainder" variable
+
+
+# Print the results of the arithmetic operations
+# What command will you use to print the results?
+
+#echo "Your favorite number doubled is: ________"
+#echo "The square of your favorite number is: ________"
+#echo "The remainder when your favorite number is divided by 2 is: ________"
+
+# 4. Conditions
+# Check if the favorite number is even or odd
+
+
+# 5. Case Statements
+
+# Categorize the favorite number
+
+#echo "Let's categorize your favorite number:"
+
+
+# 6. Shell Loops
+
+# Loop through numbers from 1 to the favorite number
+# How would you write a for loop to iterate from 1 to the user's number?
+#echo "Counting from 1 to your favorite number:"
+
+
+# 7. Shell Functions
+
+# Define a simple function to say goodbye his name is "say_goodbye"
+# What is the syntax to define a function in BASH?
+
+
+# Call the function to say goodbye
+
+________
